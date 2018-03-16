aad325dc9194ce8443a322b76709e7f14f03306a
shun <1018zaqzaq1224@gmail.com>
2018-03-16 16:30:10 +0900

    20180316_2

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,7 +4,8 @@ $(function(){
 	$('.snapshotimage').slick();
 
 	$('section.home.open').bgSwitcher({
-		images: ['assets/top.jpg','assets/top2.jpg','assets/top3.jpg','assets/top4.jpg','assets/top5.jpg','assets/top6.jpg'],
+		// images: ['assets/top.jpg','assets/top2.jpg','assets/top3.jpg','assets/top4.jpg','assets/top5.jpg','assets/top6.jpg'],
+		images: ['/top.jpg', '/top2.jpg', '/top3.jpg'],
 		loop: true,
 		shuffle: true,
 	});
